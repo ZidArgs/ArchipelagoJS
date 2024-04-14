dd1e09107819eea185c958df51f3347786db23ce
Denis Weiß <zidargs@gmail.com>
2024-04-14 18:24:52 +0200

    update license; update eslint config;

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import WebService from "webservice/WebService.js";
-import StaticService from "webservice/services/StaticService.js";
+import WebService from "jswebservice/WebService.js";
+import StaticService from "jswebservice/services/StaticService.js";
 
 const enableCors = process.argv.indexOf("-cors") >= 1;
 const port = process.argv.indexOf("-port") >= 1 ? process.argv[process.argv.indexOf("-port") + 1] : "5700";
